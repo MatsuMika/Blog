14f43f9b47c1cc7066db77df84448da31f79b0c7
Mika <risa.mkwork314@gmail.com>
2021-06-08 18:02:13 +0900

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        /**
+         * 18行目では、本登録しなくてもログインできてしまうため、 + erifiedに変更
+        * $this->middleware('auth');
+        */    
+        $this->middleware('verified');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+}
---
+++ app/User.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable implements MustVerifyEmail
+{
+    
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    /* + 22行目プロフィール画像 */
+    protected $fillable = [
+        'name', 'email', 'password', 'thumbnail'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+
+    /* + 42行目デフォルトプロフィール画像 */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'thumbnail' => 'default.png',
+    ];
+}

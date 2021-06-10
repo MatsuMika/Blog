16ac768bd76401aaca12ee7898053094ff1f5bff
Mika <risa.mkwork314@gmail.com>
2021-06-10 15:02:34 +0900

    post_root

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,18 @@ Route::get('/', function () {
 
 Auth::routes(['verify' => true]);
 
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/posts', 'PostController@index')->name('posts.index');
+Route::get('/posts/create', 'PostController@create')->name('posts.create');
+
+//Route::get('/comments', 'CommentController');
+
 Route::get('profile', function () {
     // Only verified users may enter...
 })->middleware('verified');
 
-Route::get('/home', 'HomeController@index')->name('home');
+
+
 
 //Route::post('/mypage', 'Auth\TimelineController@storeMyImg');
